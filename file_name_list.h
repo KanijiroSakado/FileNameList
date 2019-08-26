5b8600033f70a5d3964c6996b3126cbb8743de00
Kojiro Sakado <47074373+KojiroSakado@users.noreply.github.com>
2019-08-26 15:23:04 +0900

    Add files via upload

---
+++ file_name_list.h
@@ -0,0 +1,54 @@
+#ifdef FILE_NAME_LIST_H
+#else
+#define FILE_NAME_LIST_H
+
+#include <filesystem>
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+namespace sakado {
+
+	class FileNameList {
+
+	public:
+		vector<string> FileNames;
+		vector<string> DirNames;
+		vector<string> FilePath;
+		vector<string> DirPath;
+		vector<FileNameList> Dirs;
+		string Path;
+
+		FileNameList() {
+			FileNameList_Private(".");
+		}
+
+		FileNameList(string targetPath) {
+			FileNameList_Private(targetPath);
+		}
+
+	private:
+
+		void FileNameList_Private(string targetPath) {
+			Path = targetPath;
+			//ディレクトリ直下に含まれる全ファイル
+			for (const filesystem::directory_entry& x : filesystem::directory_iterator(targetPath)) {
+				if (x.is_directory()) {
+					DirNames.push_back(x.path().filename().string());
+					DirPath.push_back(Path + "/" + x.path().filename().string());
+				}
+				else {
+					FileNames.push_back(x.path().filename().string());
+					FilePath.push_back(Path + "/" + x.path().filename().string());
+				}
+			}
+			for (string str : DirNames) {
+				Dirs.push_back(*(new FileNameList(targetPath + "/" + str)));
+			}
+		}
+
+	};
+
+}
+#endif
