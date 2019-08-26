a90f0df8f80daa8b28a09cd572631bbbac4e04bb
Kojiro Sakado <47074373+KojiroSakado@users.noreply.github.com>
2019-08-26 15:20:57 +0900

    Add files via upload

---
+++ sample.cpp
@@ -0,0 +1,21 @@
+#include "file_name_list.h"
+
+using namespace std;
+
+int main() {
+
+	sakado::FileNameList fnl("./test");
+
+	cout <<"Target Directory : "<< fnl.Path<<"\n";
+	cout << "\nFiles :\n";
+	for (string str : fnl.FileNames)cout << str << "\n";
+	cout << "\nDirectories :\n";
+	for (string str : fnl.DirNames)cout << str << "\n";
+	cout << "\nFile paths :\n";
+	for (string str : fnl.FilePath)cout << str << "\n";
+	cout << "\nDirectory paths :\n";
+	for (string str : fnl.DirPath)cout << str << "\n";
+	
+	int dummy;
+	cin >> dummy;
+}
